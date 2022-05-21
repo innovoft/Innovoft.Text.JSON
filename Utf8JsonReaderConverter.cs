c3b3fdda84152ca4ee3b619f55b024984926680a
Steven Retz <sretz@innovoft.com>
2022-05-21 11:47:51 -0300

    started class public static class Utf8JsonReaderConverter

---
+++ Utf8JsonReaderConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace Innovoft.Text.JSON
+{
+	public static class Utf8JsonReaderConverter
+	{
+		#region Methods
+		#endregion //Methods
+	}
+}
