5db6e036c141134b6c9e018d3905e895b2e0648e
Steven Retz <sretz@innovoft.com>
2022-05-21 11:54:25 -0300

    added method public static bool TryGetInt16(ref Utf8JsonReader reader, out short value)

--- Utf8JsonReaderConverter.cs
+++ Utf8JsonReaderConverter.cs
@@ -99,6 +99,22 @@ namespace Innovoft.Text.JSON
 			}
 		}
 
+		public static bool TryGetInt16(ref Utf8JsonReader reader, out short value)
+		{
+			switch (reader.TokenType)
+			{
+			case JsonTokenType.String:
+				return Utf8Parser.TryParse(reader.ValueSpan, out value, out var consumed);
+
+			case JsonTokenType.Number:
+				return reader.TryGetInt16(out value);
+
+			default:
+				value = default;
+				return false;
+			}
+		}
+
 		public static void Skip(ref Utf8JsonReader reader)
 		{
 			if (reader.TokenType == JsonTokenType.PropertyName)
